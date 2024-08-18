bebe1b98872b360166744a1da6167cbc24bd7093
wolf2234 <viktoral9599@gmail.com>
2024-08-18 21:16:21 +0300

    first commit

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const EditTodoForm = ({ editTodo, task }) => {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        editTodo(value, task.id);
+        setValue("");
+    };
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="todo-input"
+                value={value}
+                placeholder="Update Task"
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit" className="todo-btn">
+                Update Task
+            </button>
+        </form>
+    );
+};
+
+export default EditTodoForm;
